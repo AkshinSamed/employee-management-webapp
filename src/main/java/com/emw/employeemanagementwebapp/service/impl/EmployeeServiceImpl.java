31834722292fedf634d1ea27f3e2c142b6f6b670
AkshinSamed <samedliakshin@gmail.com>
2022-03-18 22:35:36 +0400

    Implemented list employee feature

---
+++ src/main/java/com/emw/employeemanagementwebapp/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,21 @@
+package com.emw.employeemanagementwebapp.service.impl;
+
+import com.emw.employeemanagementwebapp.model.Employee;
+import com.emw.employeemanagementwebapp.repository.EmployeeRepository;
+import com.emw.employeemanagementwebapp.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
