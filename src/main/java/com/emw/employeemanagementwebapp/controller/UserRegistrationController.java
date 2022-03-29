a4fb92024d9f11a400a51be54144d765d5d1238e
AkshinSamed <samedliakshin@gmail.com>
2022-03-30 00:55:53 +0400

    frontend implementation for registration feature

--- src/main/java/com/emw/employeemanagementwebapp/controller/UserRegistrationController.java
+++ src/main/java/com/emw/employeemanagementwebapp/controller/UserRegistrationController.java
@@ -4,7 +4,9 @@ import com.emw.employeemanagementwebapp.dto.UserRegistrationDTO;
 import com.emw.employeemanagementwebapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,6 +16,18 @@ public class UserRegistrationController {
     @Autowired
     private UserService userService;
 
+    @ModelAttribute("user")
+    public UserRegistrationDTO userRegistrationDTO(){
+        return new UserRegistrationDTO();
+    }
+
+    @GetMapping("/registration")
+    public String showRegistrationForm(){
+        return "registration";
+    }
+
+
+    @PostMapping("/registration")
     public String registerUserAccount(@ModelAttribute("user")UserRegistrationDTO registrationDTO){
         userService.save(registrationDTO);
         return "redirect:/registration?success";
