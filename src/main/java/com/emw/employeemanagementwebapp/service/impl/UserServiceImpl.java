3aec59bda02e3c90299a46415ea0c6cb9b50b5ae
AkshinSamed <samedliakshin@gmail.com>
2022-04-03 22:16:57 +0400

    backend implementation for login feature

--- src/main/java/com/emw/employeemanagementwebapp/service/impl/UserServiceImpl.java
+++ src/main/java/com/emw/employeemanagementwebapp/service/impl/UserServiceImpl.java
@@ -6,11 +6,16 @@ import com.emw.employeemanagementwebapp.model.User;
 import com.emw.employeemanagementwebapp.repository.UserRepository;
 import com.emw.employeemanagementwebapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -18,12 +23,15 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     @Override
     public User save(UserRegistrationDTO registrationDTO) {
         User user = new User(registrationDTO.getFirstName(),
                              registrationDTO.getLastName(),
                              registrationDTO.getEmail(),
-                             registrationDTO.getPassword(),
+                             passwordEncoder.encode(registrationDTO.getPassword()),
                              List.of(new Role("ROLE_USER")));
 
         return userRepository.save(user);
@@ -31,6 +39,18 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository.findUserByEmail(username);
+        if (user == null){
+            throw new UsernameNotFoundException("Invalid username or password");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),
+                                                                      user.getPassword(),
+                                                                      mapRolesToAuthorities(user.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles){
+        return roles.stream()
+                    .map(role -> new SimpleGrantedAuthority(role.getName()))
+                    .collect(Collectors.toList());
     }
 }
